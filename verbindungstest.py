ce8a7f0a4dc030c32c20f67ba5002b0d66bf38a5
Marcinator2 <marcinator_2@gmx.de>
2023-08-26 05:54:20 +0200

    Mein features work

--- verbindungstest.py
+++ verbindungstest.py
@@ -1,13 +1,18 @@
 import pandas as pd
 
+# # Hier kannst du Funktionen oder Klassen aus datei2 aufrufen
+### datei2.meine_funktion()
 # Define the CSV file path
-csv_file_path = "./ConnectionTest_german.csv"  # Replace with the actual path to your CSV file
+def machen(csv_file_path):
+   # csv_file_path = "./ConnectionTest_german.csv"  # Replace with the actual path to your CSV file
 
-# Read the CSV and set the header
-header_row = 2  # 0-based index of the row containing the header
-df = pd.read_csv(csv_file_path, header=header_row)
+    # Read the CSV and set the header
+    header_row = 2  # 0-based index of the row containing the header
+    df = pd.read_csv(csv_file_path, header=header_row)
 
-# Save the DataFrame to an Excel file
-excel_file_path = './Verbindungstest.xlsx'  # Replace with the desired path for the Excel file
-df.to_excel(excel_file_path, index=False)
-#print(df)
+    # Save the DataFrame to an Excel file
+    excel_file_path  = csv_file_path[:-3] +"xlsx"          #'./output_with_plot.xlsx'
+
+
+    df.to_excel(excel_file_path, index=False)
+    #print(df)
